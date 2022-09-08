8299c3f6dfe4ad851687fdcd0fcb9ce25a852c9d
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-09-08 20:23:34 +0200

    task - 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
+/** @format */
+
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const elements = ingredients.map((el) => {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = el;
+  return itemEl;
+});
+
+ingredientsEl.append(...elements);
