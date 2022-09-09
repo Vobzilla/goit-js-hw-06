6a4b441a87d9d192e34ff68fc2b931a1b2ea0eab
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-09-09 20:32:10 +0200

    task - 09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+/** @format */
+const buttonEl = document.querySelector(".change-color");
+const textColorEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+buttonEl.addEventListener("click", getRandomHexColor);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  textColorEl.textContent = randomColor;
+  bodyEl.style.backgroundColor = randomColor;
 }
