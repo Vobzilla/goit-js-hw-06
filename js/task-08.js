5404552538d822dbfde398d476e7422c5a12dc0d
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-09-09 20:13:12 +0200

    task - 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+/** @format */
 
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements[0].value;
+  const password = formElements[1].value;
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  if (mail === "" || password === "") {
+    return alert("Всі поля повинні бути заповнені!!!!");
+  }
+  console.log(formData);
+  event.currentTarget.reset();
+}
