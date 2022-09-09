1875fe9824630a1cfcf7ea573a17053bfcce15d6
Volodymyr Tymoshchuk <tymvol367@gmail.com>
2022-09-09 11:30:04 +0200

    task - 05

---
+++ js/task-05.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const inputEl = document.querySelector("#name-input");
+const nameEl = document.querySelector("#name-output");
+
+inputEl.addEventListener("input", onInputChange);
+
+function onInputChange(event) {
+  nameEl.textContent = event.currentTarget.value;
+  if (event.currentTarget.value === "") {
+    nameEl.textContent = "Anonymous";
+  }
+}
